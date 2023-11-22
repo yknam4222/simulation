932dfdfb17c45596505c7faf20614722966d031e
yknam4222 <yyhnam2222@gmail.com>
2023-11-23 04:23:46 +0900

    111

--- Simulation/Assets/SimulationManager.cs
+++ Simulation/Assets/SimulationManager.cs
@@ -1,18 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SimulationManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Toggle modelAToggle;
+    public Toggle modelBToggle;
+    public Button startButton;
+    public Button pauseResumeButton;
+    public Slider timeScaleSlider;
+    public Text pauseResumeText;
+
+    public bool simulationRunning = false;
+
+    private SpeedControl speedControl;
+
     void Start()
     {
-        
+        startButton.onClick.AddListener(StartSimulation);
+        pauseResumeButton.onClick.AddListener(TogglePauseResume);
+        timeScaleSlider.onValueChanged.AddListener(ChangeTimeScale);
+        speedControl = GameObject.Find("Slider").GetComponent<SpeedControl>();
+        Time.timeScale = 0f;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // 여기에 필요한 다른 업데이트 로직을 추가하세요.
+    }
+
+    void StartSimulation()
+    {
+        // 둘 다 선택되었거나 둘 다 선택되지 않았을 경우 실행하지 않음
+        if ((modelAToggle.isOn && modelBToggle.isOn) || (!modelAToggle.isOn && !modelBToggle.isOn))
+        {
+            Debug.Log("Select one model before starting the simulation.");
+            return;
+        }
+
+        // 여기에 시뮬레이션 시작 로직 추가
+        Time.timeScale = 1f;
+        simulationRunning = true;
+        // 시뮬레이션 시작 후 버튼 상태 업데이트
+        startButton.interactable = false;
+        modelAToggle.interactable = false;
+        modelBToggle.interactable = false;
+
+        speedControl.sliderinteractable = true;
+        pauseResumeButton.interactable = true;
+    }
+
+    void TogglePauseResume()
+    {
+        if (simulationRunning)
+        {
+            // 시뮬레이션 일시 중지
+            Time.timeScale = 0f;
+            pauseResumeText.text = "Resume";
+        }
+        else
+        {
+            // 시뮬레이션 재개
+            if (speedControl.speedSlider.value == 0)
+                Time.timeScale = 1f;
+            else
+                Time.timeScale = speedControl.speedSlider.value;
+
+            pauseResumeText.text = "Pause";
+        }
+
+        // 시뮬레이션 상태 업데이트
+        simulationRunning = !simulationRunning;
+        speedControl.sliderinteractable = simulationRunning;
+    }
+
+    void ChangeTimeScale(float newScale)
+    {
+        if (simulationRunning)
+        {
+            // 시뮬레이션 중일 때만 Time.timeScale 조절
+            Time.timeScale = newScale;
+        }
     }
 }
--- Simulation/Assets/SpeedControl.cs
+++ Simulation/Assets/SpeedControl.cs
@@ -7,20 +7,21 @@ public class SpeedControl : MonoBehaviour
 {
     public Slider speedSlider; // UI Slider를 연결할 변수
     public Text timeScaleText; // UI Text를 연결할 변수
+    private SimulationManager simulationManager;
 
+    public bool sliderinteractable;
     void Start()
     {
         // Slider에 이벤트 리스너 추가
         speedSlider.onValueChanged.AddListener(ChangeSpeed);
-
         // 초기 Text 값을 설정
         UpdateTimeScaleText();
+        sliderinteractable = false;
     }
 
     void Update()
     {
-        // 실제로는 이 부분에 게임 로직이 들어갑니다.
-        // Time.deltaTime을 사용하는 모든 부분이 속도 조절의 영향을 받습니다.
+        speedSlider.interactable = sliderinteractable;
     }
 
     // Slider 값이 변경될 때 호출되는 메서드
